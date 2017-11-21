fd96aee2e968d3f37ba0b7b025f34edbdbe57cb0
Chanakar <bmcrathnayaka@gmail.com>
2017-11-21 16:46:29 +0530

    added apache http client functionalities to send embedded JSON in a POST

--- src/DruidDataService.java
+++ src/DruidDataService.java
@@ -1,5 +1,26 @@
+
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 /**
  * Created by inocer on 11/20/17.
@@ -68,4 +89,44 @@ public class DruidDataService extends DataService {
     public double[] getIntegerValuesForClassValuesArray(String attribute, String classValue, String classAttribute, int count, String table, Connection connection) {
         return new double[0];
     }
+
+    public void whenPostJsonUsingHttpClient_thenCorrect()
+            throws ClientProtocolException, IOException {
+
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://localhost:8082/druid/v2/?pretty");
+
+//        HttpClient client = new DefaultHttpClient();
+//        HttpPost httpPost = new HttpPost(
+//                " http://localhost:8082/druid/v2/?pretty");
+        String json = "{\n" +
+                "  \"queryType\" : \"topN\",\n" +
+                "  \"dataSource\" : \"wikiticker\",\n" +
+                "  \"intervals\" : [\"2015-09-12/2015-09-13\"],\n" +
+                "  \"granularity\" : \"all\",\n" +
+                "  \"dimension\" : \"page\",\n" +
+                "  \"metric\" : \"edits\",\n" +
+                "  \"threshold\" : 25,\n" +
+                "  \"aggregations\" : [\n" +
+                "    {\n" +
+                "      \"type\" : \"longSum\",\n" +
+                "      \"name\" : \"edits\",\n" +
+                "      \"fieldName\" : \"count\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+
+        CloseableHttpResponse response =  client.execute(httpPost);
+
+        String json_string = EntityUtils.toString(response.getEntity());
+        System.out.printf(json_string);
+
+    }
+
 }
+
+
--- src/TestMain.java
+++ src/TestMain.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+
 /**
  * Created by inocer on 11/20/17.
  */
 public class TestMain {
+    public static void main(String[] args) {
+        DruidDataService d = new DruidDataService();
+        try {
+            d.whenPostJsonUsingHttpClient_thenCorrect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
